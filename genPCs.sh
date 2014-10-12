2d37ba11cf0bbc01b6554d64f6e7a7529bc82dac
Christian Kastner <ckaestne@feature.isri.cmu.edu>
2014-10-12 11:55:36 -0400

    update genPCs script now using modified KBuildMiner
    using own fork of the project with different frontend

--- genPCs.sh
+++ genPCs.sh
@@ -3,14 +3,17 @@
 
 thisdir=`pwd`
 echo $thisdir
-kbuildminerDir=../BusyboxAnalysis/KBuildMiner
+linuxdir=$thisdir/l
+kbuildminerDir=../KBuildMiner
 
 cd $kbuildminerDir
 for arch in x86 arm alpha  avr32   cris  h8300 m68k   microblaze  mn10300  powerpc  score  sparc    xtensa  blackfin  frv   ia64   m32r     m68knommu  mips        parisc   s390 sh     um
 do
- mvn scala:run -q -DmainClass=gsd.buildanalysis.linux.KBuildMinerMain "-DaddArgs=--codebase|$thisdir/linux-2.6.33.3|--linuxArch|$arch|--pcOutput|$thisdir/pcs/$arch.pc"
+ cp $linuxdir/arch/$arch/Makefile $linuxdir/tmpMake
+ ./run.sh gsd.buildanalysis.linux.KBuildMinerMain --codebase $linuxdir --topFolders "tmpMake,block,crypto,drivers,firmware,fs,init,ipc,kernel,lib,mm,net,security,sound" --pcOutput $thisdir/pcs/$arch.pc
  
- cat $thisdir/pcs/$arch.pc | grep "^arch" | sed 's/\(.*\).[cS]:.*/\1/' > $thisdir/pcs/$arch.flist
+ cat $thisdir/pcs/$arch.pc | grep "\.c: " | sed 's/\(.*\).[cS]:.*/\1/' > $thisdir/pcs/$arch.flist
+ exit
 done
 
 cd $thisdir
